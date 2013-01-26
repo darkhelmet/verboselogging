2b36f5218f8da08f5ba26c7c19cec7904239ccc4
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2013-01-25 21:26:00 -0700

    omg more test stuff

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@ set -e
 export GOPATH=$PWD
 
 function go_packages() {
-    find . -iname '*.go' -exec dirname {} \; | sort | uniq
+    find . -iname '*.go' -exec dirname {} \; | sort | uniq | grep -v verboselogging
 }
 
 (
     cd src
     go_packages | while read pkg; do go test $pkg; done
 )
+
+go test -c verboselogging && ./verboselogging.test

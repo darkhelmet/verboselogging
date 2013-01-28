4eb6cb4e5ca33d0f5db7f5a90acbb8e3361a3dc7
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2013-01-27 18:24:39 -0700

    Use proper time zones...

--- test.sh
+++ test.sh
@@ -3,13 +3,17 @@ set -e
 export GOPATH=$PWD
 
 IGNORE='verboselogging|gocheck'
-function go_packages() {
+go_packages() {
     find . -iname '*.go' -exec dirname {} \; | sort | uniq | grep -v -E $IGNORE
+}
+
+test_packages() {
+    while read pkg; do go test $pkg; done
 }
 
 (
     cd src
-    go_packages | while read pkg; do go test $pkg; done
+    go_packages | test_packages
 )
 
-go test -c verboselogging && ./verboselogging.test
+GOPATH=$PWD go test -c verboselogging && ./verboselogging.test
